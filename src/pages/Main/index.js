d27bd4431733dd662241c01bfdec017d934bab13
Luiz Filipe <infobase.lfsilva@terceiros.mag.com.br>
2021-11-22 22:32:56 -0300

    styling the home screen

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
 const Main = () => {
   return (
-      <Title>
-        This is a Main component
-      </Title>
+      <Container>
+          <h1>
+              <FaGithub size={ 25 } />
+              Meus repositórios
+          </h1>
+
+          <Form onSubmit={ () => {} }>
+              <input
+                  type='text'
+                  placeholder='Adicionar repositórios'
+              />
+
+              <SubmitButton>
+                  <FaPlus color='#fff' size={ 14 } />
+              </SubmitButton>
+          </Form>
+      </Container>
   );
 };
 
